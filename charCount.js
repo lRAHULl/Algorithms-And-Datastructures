e7092d88d90a02a98e903a12d63094b0145041a2
lRAHULl <rahulpadmaraju16@gmail.com>
2018-12-08 19:47:30 +0530

    Practiced some Algorithms from Hackkerank

--- charCount.js
+++ charCount.js
@@ -2,12 +2,12 @@ function charCount(str) {
 	var result = {};
 	for (let i = 0; i < str.length; i++) {
 		char = str[i].toLowerCase();
-		// notChar = " !@#$%^&*()_+}{:?><~``[];',./?\|";
-		// for (let i = 0; i < notChar.length; i++) {
-		// 	if (char === notChar[i]) {
-		// 		char = null;
-		// 	}
-		// }
+		notChar = " !@#$%^&*()_+}{:?><~``[];',./?\|";
+		for (let i = 0; i < notChar.length; i++) {
+			if (char === notChar[i]) {
+				char = null;
+			}
+		}
 		if (result[char] > 0) {
 			result[char]++;
 		} else {

ce6107f8820ecdc06776449092829c037fe4a6c3
lRAHULl <rahulpadmaraju16@gmail.com>
2018-12-25 17:02:51 +0530

    Learnt Singly Linked Lists

--- changeBits.py
+++ changeBits.py
@@ -1,33 +1,78 @@
-def changeBits(a, b, queries):
-    res = ''
-    for x in queries:
-            queryArr = x.split(' ')
-            query = queryArr[0]
-            idx = int(queryArr[1])
-            # print(queryArr)
-            if query == 'set_a':
-                print('a: ', a)
-                x = query[2]
-                print('x: ', x)
-                a = a.replace(a[idx], x, 1)
+# def setbit(val, i, bit):
 
-                # a[idx] = x
-            elif query == 'set_b':
+#     num = 1 << i
 
-                x = query[2]
+#     if bit:
+#         return val | num
 
-                b.replace(b[idx], x, 1)
+#     return val & ~num
+# def changeBits(a, b, queries):
+#     res = ''
+#     for x in queries:
+#             queryArr = x.split(' ')
+#             query = queryArr[0]
+#             idx = int(queryArr[1])
+#             # print(queryArr)
+#             if query == 'set_a':
+#                 print('a: ', a)
+#                 x = int(query[2])
+#                 print('x: ', x)
+#                 a = setbit(a, idx, x)
 
-            elif query == 'get_c':
+#                 # a[idx] = x
+#             elif query == 'set_b':
 
-                c = a + b
+#                 x = int(query[2])
 
-                temp = c[idx]
+#                 b = setbit(b, idx, x)
 
-                res += temp
+#             elif query == 'get_c':
 
-    print(res)
+#                 c = a + b
+
+#                 cbit = int (bool (c & (1<<inx)))
+
+#                 res += str(temp)
+
+#     print(res)
 
 
-changeBits('00000', '11111', ['set_a 0 1',
-                              'get_c 5', 'get_c 1', 'set_b 2 0', 'get_c 5'])
+# changeBits('00000', '11111', ['set_a 0 1','get_c 5', 'get_c 1', 'set_b 2 0', 'get_c 5'])
+
+
+def setbit(val, i, bit):
+
+    num = 1 << i
+
+    if bit:
+        return val | num
+
+    return val & ~num
+
+n, q = map(int, input().strip().split(' '))
+a = int(input().strip(),2)
+b = int(input().strip(),2)
+out = ''
+
+for i in range(q):
+
+    cmd = input().strip().split(' ')
+    inx = int(cmd[1])
+    cmd.append(7)
+    bit = int(cmd[2])
+
+    if cmd[0] =='set_a':
+
+        a = setbit(a, inx, bit)
+
+    elif cmd[0] =='set_b':
+
+        b = setbit(b, inx, bit)
+
+    else:
+
+        c = a+b
+        cbit = int (bool (c & (1<<inx)))
+        out += str(cbit)
+
+print (out)

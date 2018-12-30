4458f4496c541b1c36a06280cd502107fb4af03b
lRAHULl <rahulpadmaraju16@gmail.com>
2018-12-30 15:54:50 +0530

    Binary Heaps going

--- Data Structures/BinaryHeaps.js
+++ Data Structures/BinaryHeaps.js
@@ -1,14 +1,35 @@
-function odd2(arr) {
-  let obj = {};
-  for (let x of arr) {
-    obj[x] ? (obj[x] += 1) : (obj[x] = 1);
+/**
+ * 
+ * 
+ * 
+ * 
+ */
+
+class MaxBinaryHeap {
+  constructor() {
+    this.values = [];
   }
-  for (let x in obj) {
-    if (obj[x] % 2 !== 0) {
-      console.log(x);
+  insert(value) {
+    this.values.push(value);
+    this.bubbleUp();
+    console.log(this.values);
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const value = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (value <= parent) break;
+      this.values[idx] = parent;
+      this.values[parentIdx] = value;
+      idx = parentIdx;
     }
   }
-  console.log(0);
 }
 
-odd2([4, 4, 8, 8, 23]);
+let heap = new MaxBinaryHeap();
+heap.insert(10);
+heap.insert(20);
+heap.insert(30);
+heap.insert(3);

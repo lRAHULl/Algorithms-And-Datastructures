8d5fc4e0c741513ac6d4c4f338c828d15b43fe2e
lRAHULl <rahulpadmaraju16@gmail.com>
2019-01-02 15:30:40 +0530

    extract Max from Max Binary heap implemented

--- Fundamentals/Data Structures/BinaryHeaps.js
+++ Fundamentals/Data Structures/BinaryHeaps.js
@@ -4,18 +4,20 @@
  * L-Child -> 2n + 1
  * R-child -> 2n + 2
  * 
- * child at n -> parent ar int((n-1)/2)
+ * child at n -> parent at int((n-1)/2)
  */
 
 class MaxBinaryHeap {
   constructor() {
     this.values = [];
   }
+
   insert(value) {
     this.values.push(value);
     this.bubbleUp();
     return this;
   }
+
   bubbleUp() {
     let idx = this.values.length - 1;
     let element = this.values[idx];
@@ -32,6 +34,50 @@ class MaxBinaryHeap {
       }
     }
   }
+
+  extractMax() {
+    let max = this.values[0];
+    let end = this.values.pop();
+    if (this.values.length > 0) {
+      this.values[0] = end;
+      this.sinkDown();
+    }
+    return max;
+  }
+
+  sinkDown() {
+    let idx = 0;
+    let element = this.values[0];
+    let length = this.values.length;
+    while (true) {
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftChildIdx < length) {
+        leftChild = this.values[leftChildIdx];
+        if (leftChild > element) {
+          swap = leftChildIdx;
+        }
+      }
+      if (rightChildIdx < length) {
+        rightChild = this.values[rightChildIdx]
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightChildIdx;
+        }
+      }
+      if (swap === null) {
+        break;
+      }
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
 }
 
 let heap = new MaxBinaryHeap();
@@ -39,4 +85,8 @@ heap.insert(10);
 heap.insert(20);
 heap.insert(30);
 heap.insert(5);
-console.log(heap.values)
+console.log(heap.extractMax());
+console.log(heap.extractMax());
+console.log(heap.extractMax());
+console.log(heap.extractMax());
+console.log(heap.values);

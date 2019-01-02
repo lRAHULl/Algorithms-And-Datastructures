327f093fa70def76e5803a5da71234dd0baf6254
lRAHULl <rahulpadmaraju16@gmail.com>
2019-01-02 21:08:31 +0530

    Weighted Graphs

---
+++ Fundamentals/Data Structures/Dijktra'sAlgorithms.js
@@ -0,0 +1,32 @@
+class WeightedGraph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+
+    addVertex(vertex) {
+        if (!(vertex in this.adjacencyList)) {
+            this.adjacencyList[vertex] = [];
+        }
+    }
+
+    addEdge(vertex1, vertex2, weight) {
+        if (vertex1 && vertex2 in this.adjacencyList) {
+            this.adjacencyList[vertex1].push({
+                node: vertex2,
+                weight
+            });
+            this.adjacencyList[vertex2].push({
+                node: vertex1,
+                weight
+            });
+        }
+    }
+}
+
+let g = new WeightedGraph();
+g.addVertex('A');
+g.addVertex("B");
+g.addVertex("C");
+g.addEdge("A", "B", 10);
+g.addEdge("C", "B", 5);
+console.log(g.adjacencyList);
---
+++ Fundamentals/Data Structures/WeightedGraph.js
@@ -0,0 +1,30 @@
+class WeightedGraph {
+    constructor() {
+        this.adjacencyList = {};
+    }
+
+    addVertex(vertex) {
+        if (!(vertex in this.adjacencyList)) {
+            this.adjacencyList[vertex] = [];
+        }
+    }
+
+    addEdge(vertex1, vertex2, weight) {
+        if (vertex1 && vertex2 in this.adjacencyList) {
+            this.adjacencyList[vertex1].push({
+                node: vertex2,
+                weight
+            });
+            this.adjacencyList[vertex2].push({
+                node: vertex1,
+                weight
+            });
+        }
+    }
+}
+
+let g = new WeightedGraph();
+g.addVertex('A');
+g.addVertex("B");
+g.addEdge("A", "B", 10);
+console.log(g.adjacencyList);
